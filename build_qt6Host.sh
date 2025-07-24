a8dbc012fdb50cedac105a4fa3421214954b2b43
Christopher Radoumis <cradoumis@MacBookPro.domain.local>
2025-07-24 10:35:51 -0700

    Reducing to minimal build

--- build_qt6Host.sh
+++ build_qt6Host.sh
@@ -5,137 +5,114 @@
 
 set -exuo pipefail
 
-BUILD_TARGET=/build/qt-hostbuild
 SRC=/src
 QT_BRANCH_MAJOR="6.9"
 QT_BRANCH_MINOR="1"
 DEBIAN_VERSION=$(lsb_release -cs)
 MAKE_CORES="$(expr $(nproc))"
+BUILD_TARGET_PI=/build/qtpi-build
+BUILT_TARGET_PI=/built
 
-mkdir -p "$BUILD_TARGET"
-mkdir -p "$SRC"
+mkdir -p "$BUILT_TARGET_PI"
+mkdir -p "$BUILD_TARGET_PI"
 
 /usr/games/cowsay -f tux "Building QT version $QT_BRANCH_MAJOR.$QT_BRANCH_MINOR."
 
-function fetch_qt6 () {
-
-    /usr/games/cowsay -f tux "Fetching QT $QT_BRANCH_MAJOR.$QT_BRANCH_MINOR."
-    
+function build_qtpi () {
     local SRC_DIR="$SRC/qt6"
-    pushd $SRC
 
-    if [ ! -d "$SRC_DIR" ]; then
-        mkdir -p "$SRC_DIR"
+    pushd "$BUILD_TARGET_PI"
 
-        wget --no-check-certificate -q --progress=bar:force:noscroll --show-progress "https://download.qt.io/official_releases/qt/$QT_BRANCH_MAJOR/$QT_BRANCH_MAJOR.$QT_BRANCH_MINOR/single/qt-everywhere-src-$QT_BRANCH_MAJOR.$QT_BRANCH_MINOR.tar.xz"
-        pv qt-everywhere-src-$QT_BRANCH_MAJOR.$QT_BRANCH_MINOR.tar.xz | tar xpJ -C "$SRC_DIR" --strip-components=1
-        rm qt-everywhere-src-$QT_BRANCH_MAJOR.$QT_BRANCH_MINOR.tar.xz
+    cmake $SRC_DIR -GNinja \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DINPUT_opengl=es2 \
+        -DQT_FEATURE_kms=ON \
+        -DQT_FEATURE_opengles2=ON \
+        -DQT_FEATURE_opengles3=ON \
+        -DQT_FEATURE_jpeg=OFF \
+        -DQT_FEATURE_eglfs=ON \
+        -DQT_FEATURE_xcb=ON \
+        -DQT_FEATURE_xlib=ON \
+        -DQT_FEATURE_opensource=ON \
+        -DQT_FEATURE_pkg_config=ON \
+        -DQT_FEATURE_pcre2=ON \
+        -DQT_FEATURE_precompiled_headers=OFF \
+        -DQT_FEATURE_evdev=ON \
+        -DQT_FEATURE_system_freetype=ON \
+        -DQT_FEATURE_fontconfig=ON \
+        -DQT_FEATURE_glib=ON \
+        -DQT_FEATURE_cups=OFF \
+        -DQT_FEATURE_gtk3=OFF \
+        -DQT_FEATURE_gold_linker=OFF \
+        -DQT_BUILD_EXAMPLES=OFF \
+        -DQT_BUILD_TESTS=OFF \
+        -DQT_QPA_DEFAULT_PLATFORM=eglfs \
+        -DFEATURE_xcb_xlib=ON \
+        -DBUILD_WITH_PCH=OFF \
+        -DBUILD_qtdoc=OFF \
+        -DBUILD_qttranslations=OFF \
+        -DBUILD_qtwebengine=OFF \
+        -DBUILD_qtwebview=OFF \
+        -DBUILD_qtsensors=OFF \
+        -DBUILD_qtandroidextras=OFF \
+        -DBUILD_qtgamepad=OFF \
+        -DBUILD_qtmacextras=OFF \
+        -DBUILD_qtwinextras=OFF \
+        -DBUILD_qtpurchasing=OFF \
+        -DBUILD_qtvirtualkeyboard=OFF \
+        -DBUILD_qttools=OFF \
+        -DBUILD_qtspeech=OFF \
+        -DBUILD_qtsql=OFF \
+        -DBUILD_qtxml=OFF \
+        -DBUILD_qtimageformats=OFF \
+        -DBUILD_qtlanguageserver=OFF \
+        -DBUILD_qthttpserver=OFF \
+        -DBUILD_qtserialport=OFF \
+        -DBUILD_qtlocation=OFF \
+        -DBUILD_qtlottie=OFF \
+        -DBUILD_qtmqtt=OFF \
+        -DBUILD_qtremoteobjects=OFF \
+        -DBUILD_qtserialbus=OFF \
+        -DBUILD_qtsvg=OFF \
+        -DBUILD_qtwayland=OFF \
+        -DBUILD_wayland=OFF \
+        -DBUILD_qtcoap=OFF \
+        -DBUILD_qt5compat=OFF \
+        -DBUILD_qtconnectivity=OFF \
+        -DBUILD_qtrpc=OFF \
+        -DBUILD_qtopcua=OFF \
+        -DBUILD_qtnetworkauth=OFF \
+        -DBUILD_qtactiveqt=OFF \
+        -DBUILD_qtgrpc=OFF \
+        -DBUILD_qtscxml=OFF \
+        -DBUILD_qt3d=OFF \
+        -DBUILD_qtquick3dphysics=OFF \
+        -DBUILD_qtgraphs=OFF \
+        -DQT_DEBUG_FIND_PACKAGE=ON \
+        -DQT_HOST_PATH=/build/qt-host \
+        -DCMAKE_STAGING_PREFIX=/build/qt-raspi \
+        -DCMAKE_INSTALL_PREFIX=/usr/local/qt6 \
+        -DCMAKE_PREFIX_PATH=/build/qt-host/lib/cmake \
+        -DCMAKE_TOOLCHAIN_FILE=/build/toolchain.cmake \
+        -DQT_QMAKE_TARGET_MKSPEC=devices/linux-rasp-pi4-aarch64 \
+        -DCMAKE_SYSROOT=/sysroot \
+        -DQT_AVOID_CMAKE_ARCHIVING_API=ON \
+        -DCMAKE_CXX_FLAGS="-O2"
 
-        popd
-    else
-        echo "DO NOTHING"
 
-        popd
-    fi
+    /usr/games/cowsay -f tux "Making QT Pi version $QT_BRANCH_MAJOR.$QT_BRANCH_MINOR."
 
-}
+    cmake --build . --parallel "$MAKE_CORES"
 
-function configure_qt () {
-
-    pushd "$BUILD_TARGET"
-    local SRC_DIR="/src/qt6"
-    local TAG_FILE="/usr/local/build.tag"
-
-
-    if [ ! -f "$TAG_FILE" ]; then
-
-        # Modify paths for build process
-        wget --no-check-certificate https://raw.githubusercontent.com/riscv/riscv-poky/master/scripts/sysroot-relativelinks.py -P /usr/local/bin
-        chmod +x /usr/local/bin/sysroot-relativelinks.py
-
-        /usr/local/bin/sysroot-relativelinks.py /sysroot
-
-        cmake "$SRC_DIR" -GNinja \
-            -DCMAKE_BUILD_TYPE=Release \
-            -DQT_BUILD_EXAMPLES=OFF \
-            -DINPUT_opengl=es2 \
-            -DQT_BUILD_TESTS=OFF \
-            -DCMAKE_INSTALL_PREFIX=/build/qt-host \
-            -DBUILD_qtdoc=OFF \
-            -DBUILD_qttranslations=OFF \
-            -DBUILD_qtwebengine=OFF \
-            -DBUILD_qtwebview=OFF \
-            -DBUILD_qtsensors=OFF \
-            -DBUILD_qtandroidextras=OFF \
-            -DBUILD_qtgamepad=OFF \
-            -DBUILD_qtmacextras=OFF \
-            -DBUILD_qtwinextras=OFF \
-            -DBUILD_qtpurchasing=OFF \
-            -DBUILD_qtvirtualkeyboard=OFF \
-            -DBUILD_qttools=OFF \
-            -DBUILD_qtspeech=OFF \
-            -DBUILD_qtsql=OFF \
-            -DBUILD_qtxml=OFF \
-            -DBUILD_qtimageformats=OFF \
-            -DBUILD_qtlanguageserver=OFF \
-            -DBUILD_qthttpserver=OFF \
-            -DBUILD_qtserialport=OFF \
-            -DBUILD_qtlocation=OFF \
-            -DBUILD_qtlottie=OFF \
-            -DBUILD_qtmqtt=OFF \
-            -DBUILD_qtremoteobjects=OFF \
-            -DBUILD_qtserialbus=OFF \
-            -DBUILD_qtsvg=OFF \
-            -DBUILD_qtwayland=OFF \
-            -DBUILD_wayland=OFF \
-            -DBUILD_qtcoap=OFF \
-            -DBUILD_qt5compat=OFF \
-            -DBUILD_qtconnectivity=OFF \
-            -DBUILD_qtrpc=OFF \
-            -DBUILD_qtopcua=OFF \
-            -DBUILD_qtnetworkauth=OFF \
-            -DBUILD_qtactiveqt=OFF \
-            -DBUILD_qtgrpc=OFF \
-            -DBUILD_qtscxml=OFF \
-            -DBUILD_qt3d=OFF \
-            -DBUILD_qtquick3dphysics=OFF \
-            -DBUILD_qtgraphs=OFF \
-            -DCMAKE_CXX_FLAGS="-O2"
-
-        touch "$TAG_FILE"
-
-    else
-        echo "DO NOTHING"
-    fi
-}
-
-function cmake_qt () {
-    pushd "$BUILD_TARGET"
-
-    /usr/games/cowsay -f tux "Making QT version $QT_BRANCH_MAJOR.$QT_BRANCH_MINOR. with $MAKE_CORES Cores."
-
-    cmake --build . --parallel "$MAKE_CORES" --verbose
-}
-
-function install_qt () {
-    pushd "$BUILD_TARGET"
-
-    /usr/games/cowsay -f tux "Installing QT version $QT_BRANCH_MAJOR.$QT_BRANCH_MINOR."
-
+    /usr/games/cowsay -f tux "Installing QT Pi version $QT_BRANCH_MAJOR.$QT_BRANCH_MINOR."
     cmake --install .
-
     popd
+
+    pushd /build
+    tar cfz "$BUILT_TARGET_PI/qt6pi-$QT_BRANCH_MAJOR.$QT_BRANCH_MINOR-$DEBIAN_VERSION.tar.gz" qt-raspi
+    sha256sum "$BUILT_TARGET_PI/qt6pi-$QT_BRANCH_MAJOR.$QT_BRANCH_MINOR-$DEBIAN_VERSION.tar.gz" > "$BUILT_TARGET_PI/qt6pi-$QT_BRANCH_MAJOR.$QT_BRANCH_MINOR-$DEBIAN_VERSION.tar.gz.sha256"
+    popd
+
 }
 
-
-# Get a fresh copy of QT
-fetch_qt6
-
-# Configure Qt
-configure_qt
-
-# Make Qt
-cmake_qt
-
-# Install Qt
-install_qt
+build_qtpi
